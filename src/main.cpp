c26a4211641d0c2efa99962c6867e65d1a66fd30
magnus-rex1 <barrykawooya@gmail.com>
2026-04-26 14:13:48 +0300

    testing

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,8 @@ int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
     // QApplication::setStyle(QStyleFactory::create("Fusion"));
-    QFile file(u":/forms/Darkeum/Darkeum.qss"_s);
-    // QFile file(u"forms/Combinear/Combinear.qss"_s);
+    // QFile file(u":/forms/Darkeum/Darkeum.qss"_s);
+    QFile file(u"forms/Combinear/Combinear.qss"_s);
     // QFile file(u"forms/Takezo/Takezo.qss"_s);
     if (!file.open(QIODevice::ReadOnly))
         qFatal("Cannot open resource file");
